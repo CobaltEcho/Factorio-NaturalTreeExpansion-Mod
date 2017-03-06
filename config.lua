5d27e701551eb9c78c7e91f748b942ed82e4a226
CobaltEcho <cruise.sam@gmail.com>
2017-03-06 13:12:45 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 
 -- The interval between tree grow cycle in ticks. Lower value grows faster.
-tree_expansion_frequency = 60
+tree_expansion_frequency = 10
 
 -- Show debug window for added tree counts and total counts
 enable_debug_window = false
